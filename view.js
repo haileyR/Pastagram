a5bb5fe214a80948788363132dead99f4cf498b5
HaileyR <hangyeolryu@gmail.com>
2015-07-09 13:12:54 -0700

    Delete foundation class.

--- view.js
+++ view.js
@@ -7,17 +7,22 @@ PastagramView.prototype.displayPhotos = function(photoList) {
       for (var i=0; i < photoList.length; i++) {
         var photo = photoList[i];
         html += '<li><img alt="'+ photo.title + '"src="' + "http://farm" + photo.farm + ".static.flickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + ".jpg" + '"/>' + '</li>';
+        $('#photo-grid').empty();
+        $('#photo-grid').append("<ul>" + html + "</ul>");
       };
     } else {
       for (var i=0; i < photoList.length; i++) {
         html += '<li><img alt="'+ photoList[i][1].title + '"src="' + photoList[i][1].url + '"/>' + '</li>';
+        $('#photo-grid').empty();
+        $('#photo-grid').append("<ul>" + html + "</ul>");
       };
     }
   } else {
     for (var id in photoList) {
-      html += '<li><img alt="'+ photoList[id].title + '"src="' + photoList[id].url + '"/>' + '</li>';
+      html += '<li><div class="img-container"><img alt="'+ photoList[id].title + '"src="' + photoList[id].url + '"/>' + '</div></li>';
+      $('#photo-grid').empty();
+      $('#photo-grid').append("<ul>" + html + "</ul>");
     };
   }
-  $('#photo-grid').empty();
-  $('#photo-grid').append("<ul class=\"small-block-grid-2 medium-block-grid-3 large-block-grid-4\">" + html + "</ul>");
+
 }
