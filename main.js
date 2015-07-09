17536fe58ee1a7fb81c2bd5713df44e267051da6
HaileyR <hangyeolryu@gmail.com>
2015-07-09 00:00:54 -0700

    Clean main.js by having a separate file for flickr class and methods.

--- main.js
+++ main.js
@@ -1,24 +1,7 @@
-$(document).ready(function(){
-  setSource();
-});
+flickr = new Flickr();
+flickr.getData({method: 'flickr.people.getPublicPhotos', user_id: true, jsoncallback: 'init'});
 
-function jsonFlickrApi(data) {
-  window.data = data;
-  var list = "";
-  for (var i=0; i < data.photos.photo.length; i++) {
-    photo = data.photos.photo[i];
-    photo_url = "http://farm" + photo.farm + ".static.flickr.com/" +
-      photo.server + "/" + photo.id + "_" + photo.secret + ".jpg";
-    list +=   '<li><img alt="'+ photo.title + '"src="' + photo_url + '"/>' + '</li>';
-  };
-  $('#photo-grid').append("<ul class=\"small-block-grid-2 medium-block-grid-3 large-block-grid-4\">" + list + "</ul>");
-}
-
-function setSource() {
-  var apiKey = 'a5e95177da353f58113fd60296e1d250';
-  var userID = '132365033@N08'
-  var script = document.createElement('script');
-  script.src = "https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=" + apiKey + "&user_id=" + userID + "&format=json"
-  $('head').append(script);
-  $('head').remove(script);
+function init(data){
+  group = new PhotoGroup(data);
+  group.displayPhotos();
 }
